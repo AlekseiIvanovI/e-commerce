af6b0bacb36de4e879767b20f558bc31f6e18247
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2023-12-26 21:33:21 -0800

    new commit

--- src/payload/access/admins.ts
+++ src/payload/access/admins.ts
@@ -1,26 +1,10 @@
 import type { AccessArgs } from 'payload/config'
+
 import { checkRole } from '../collections/Users/checkRole'
 import type { User } from '../payload-types'
 
 type isAdmin = (args: AccessArgs<unknown, User>) => boolean
 
 export const admins: isAdmin = ({ req: { user } }) => {
-  if (checkRole(['admin'], user)) {
-    return true; // User has the "admin" role, grant access
-  } else {
-    throw new Error('Access Denied'); // User does not have the "admin" role, deny access
-  }
+  return checkRole(['admin'], user)
 }
-
-
-
-// import type { AccessArgs } from 'payload/config'
-
-// import { checkRole } from '../collections/Users/checkRole'
-// import type { User } from '../payload-types'
-
-// type isAdmin = (args: AccessArgs<unknown, User>) => boolean
-
-// export const admins: isAdmin = ({ req: { user } }) => {
-//   return checkRole(['admin'], user)
-// }
