f1bc6ba485834ad7e72dd3139f3ebfc3d34169d8
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2023-12-26 20:09:54 -0800

    new commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,19 @@ module.exports = {
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': [ 'error', {"endOfLine" : 'auto' }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-console': 'off',
   },
-}
+};
+
+
+// module.exports = {
+//   root: true,
+//   extends: ['plugin:@next/next/recommended', '@payloadcms'],
+//   ignorePatterns: ['**/payload-types.ts'],
+//   plugins: ['prettier'],
+//   rules: {
+//     'prettier/prettier': [ 'error', {"endOfLine" : 'auto' }],
+//     'no-console': 'off',
+//   },
+// }
